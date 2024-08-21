e71192cd3baaba063a379fe966c765264e3c0912
ivania <ivaniaespejoduran@gmail.com>
2024-08-21 15:34:00 -0400

    my first commit

---
+++ carnete/src/components/carnete.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+  data() {
+    return {
+      run: '22.222.222-2',
+      apellidos: 'MUSK',
+      nombres: 'ELON REEVE',
+      nacionalidad: 'SUDAFRICANO',
+      sexo: 'M',
+      fechaNacimiento: '28 JUN 1971',
+      numeroDocumento: '222.222.222',
+      fechaEmision: '01 ENE 2025',
+      fechaVencimiento: '01 ENE 2030',
+      firmaTitular: 'X Æ A-12'
+    }
+  }
+}
+</script>
